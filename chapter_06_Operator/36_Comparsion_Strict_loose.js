68385925d287861f5cf44e83b92ebdfa83f07676
kgaurav1307 <kgaurav1307@users.noreply.github.com>
2026-05-29 00:59:29 +0530

    Add chapter 06 operator examples

---
+++ chapter_06_Operator/36_Comparsion_Strict_loose.js
@@ -0,0 +1,25 @@
+// number == string
+// console.log(42 == "42"); // == -> loose compasion
+// console.log(42 === "42"); //data type and converted value
+// console.log(42 == "45"); //value different
+
+
+// console.log(5 === 5);
+// console.log(5 === "5");
+
+// console.log(5 == 5);
+// console.log(5 == "5");
+
+// console.log(0 == ""); // ?  "" = conveted to 0 - checked by the loose
+// console.log(0 === ""); 
+
+
+console.log(true == 1);
+console.log(false == 0);
+console.log(true == "1");
+console.log(true == 2);
+
+
+console.log(5 != "5"); // false , 5 = int, "5" string, both of them are not equal? - lose couple
+console.log(5 !== "5"); // true ( value, dataType)
+//console.log(5 !=== "5"); This doesn't excit
