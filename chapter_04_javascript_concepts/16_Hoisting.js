acef254fae18d97ebaf5a3bcb7f346f1cd321027
kgaurav1307 <kgaurav1307@users.noreply.github.com>
2026-05-29 00:24:30 +0530

    Add chapter 05 literal examples and update hoisting demo in chapter 04

---
+++ chapter_04_javascript_concepts/16_Hoisting.js
@@ -0,0 +1,11 @@
+console.log(greeting);
+var greeting = "Hello";
+console.log(greeting);
+
+// console.log(a);
+// var a = "Pramod";
+// console.log(a);
+
+//output
+// undefined
+// Pramod
---
+++ chapter_05_Literal/23_null_undefined.js
@@ -0,0 +1,12 @@
+let userName;
+console.log(userName);
+console.log(typeof userName);
+
+function greet() {
+
+    // no return statement
+}
+
+console.log(greet());
+
+
